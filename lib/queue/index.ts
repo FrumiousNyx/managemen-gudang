a03ec09795fd1a2da5d2287b7e5577c0ca1ee41e
FrumiousNyx <printartful@gmail.com>
2026-09-12 15:49:25 +0700

    Remove marketplace integration and unused dependencies

--- lib/queue/index.ts
+++
@@ -1,4 +0,0 @@
-export { RateLimiter, marketplaceRateLimiter } from './rate-limiter';
-export { LocalQueue, localQueue } from './local-queue';
-export { QueueManager, queueManager } from './queue-manager';
-export type { StockSyncJob, MarketplacesToSync, LocalQueueJob } from './queue-manager';
